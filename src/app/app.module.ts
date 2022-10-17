1c12f735ec78057208b65dd4dc317bef2d2b74c5
Pedro Javier <pjpc@outlook.com.br>
2022-10-17 02:51:03 -0300

    add rate limiter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,28 @@ import { AuthModule } from 'src/shared/auth/auth.module';
 import { ApiModule } from 'src/shared/api/api.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
-    AuthModule,
-    ApiModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 100,
+    }),
+    AuthModule,
+    ApiModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
